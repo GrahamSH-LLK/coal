bc45383c1b6119a0b16b41584b1da1158ecb29fa
augustkoskinen <augustkoskinen@gmail.com>
2024-05-20 15:39:04 -0400

    oo ah AH AH ooh ah ohhh ah AHHAHAH AH

--- src/modules/coalcommands.js
+++ src/modules/coalcommands.js
@@ -28,7 +28,7 @@ defineChatCommand(
       },
     ];
 
-    await interaction.reply(`<@${options.user.id}> GET MINING!!!! ${options.user.coal} COAL TO GO!!!!!!`);
+    await interaction.reply(`<@${options.user.id}> GET MINING!!!! ${options.coal} COAL TO GO!!!!!!`);
   }
 );
 
@@ -48,7 +48,7 @@ async (interaction, options) => {
     if (record) {
         return await interaction.reply(`${record.coal} remaining`)
     }
-    return await interaction.reply( {content:`no existing punishment`,ephemeral: true})
+    return await interaction.reply( {content:`No existing punishment`,ephemeral: true})
 })
 
 defineChatCommand({
@@ -78,10 +78,10 @@ async (interaction, options) => {
             },
           ];
       
-        return await interaction.reply(`you ${won ? 'won' : 'lost'}! your coal amount is now ${newCoal}`);
+        return await interaction.reply(`You ${won ? 'won' : 'lost'}! Your coal amount is now ${newCoal}`);
 
     }
-    return await interaction.reply({content:`no existing punishment`, ephemeral: true})
+    return await interaction.reply({content:`No existing punishment`, ephemeral: true})
 })
 
 defineChatCommand({
@@ -96,9 +96,12 @@ defineChatCommand({
   }
 },
 async (interaction, options) => {
-  database.data = database.data.filter((x) => x.id != options.user.id);
-  
-  return await interaction.reply(`You freed <@${options.user.id}>! Lucky them.`)
+  let useroccurs = database.data.find((x) => x.id == options.user.id);
+  if(useroccurs) {
+    database.data = database.data.filter((x) => x.id != options.user.id);
+    return await interaction.reply(`You freed <@${options.user.id}>! Lucky them.`)
+  }
+  return await interaction.reply({content:`That user isn't mining.`, ephemeral: true})
 })
 
 defineChatCommand(
@@ -110,71 +113,42 @@ defineChatCommand(
         type: ApplicationCommandOptionType.User,
         description: "User to help",
         required: true,
-      },
-      coal: {
-        type: ApplicationCommandOptionType.Number,
-        description: "Coal to add",
-        required: true,
-      },
+      }
     },
   },
 
   async (interaction, options) => {
-    let record = database.data.find((x) => x.id == interaction.user.id)
-    if (record) {
-      if (options.amount > record.coal || options.amount < 1) {
-        return await interaction.reply( {content: `invalid amount`,ephemeral: true})
-      }
-      let newCoal = record.coal - options.user.coal;
+    let record = database.data.find((x) => x.id == options.user.id)
+    let usermining = (options.user.id == interaction.user.id)
+    let userhelping = database.data.find((x) => x.helpid && x.id == interaction.user.id)
+
+    if (record&&!usermining&&!userhelping) {
       database.data = [
-          ...database.data.filter((x) => x.id != interaction.user.id),
-          {
-            id: interaction.user.id,
-            coal: newCoal,
-          },
-        ];
-    
-      return await interaction.reply(`<@${interaction.user.id}> helped <@${options.user.id}> mine ${options.user.coal} coal! How nice. Only ${options.user.coal} coal left!`);
+        ...database.data,
+        {
+          id: interaction.user.id,
+          helpid: options.user.id
+        },
+      ];
+      return await interaction.reply(`<@${interaction.user.id}> is helping <@${options.user.id}> mine coal! How nice.`);
     }
-    return await interaction.reply({content:`Who are you even talking to?`, ephemeral: true})
+    return await interaction.reply({content: userhelping ? `You're already helping someone!` : usermining ? `You can't help yourself!` : `You can't help that user`, ephemeral: true})
   }
 );
 
 defineChatCommand(
   {
-    name: "hurtmine",
-    description: "Help a user mine",
-    options: {
-      user: {
-        type: ApplicationCommandOptionType.User,
-        description: "User to hurt",
-        required: true,
-      },
-      coal: {
-        type: ApplicationCommandOptionType.Number,
-        description: "Coal to remove",
-        required: true,
-      },
-    },
+    name: "stophelping",
+    description: "Stop helping a user"
   },
 
   async (interaction, options) => {
-    let record = database.data.find((x) => x.id == interaction.user.id)
-    if (record) {
-      if (options.amount < 1) {
-        return await interaction.reply( {content: `invalid amount`,ephemeral: true})
-      }
-      let newCoal = record.coal + options.user.coal;
-      database.data = [
-          ...database.data.filter((x) => x.id != interaction.user.id),
-          {
-            id: interaction.user.id,
-            coal: newCoal,
-          },
-        ];
-    
-      return await interaction.reply(`<@${interaction.user.id}> added ${options.user.coal} to <@${options.user.id}>'s sentence! You got another ${options.user.coal} coal left!`);
+    let record = database.data.find((x) => x.id == interaction.user.id);
+    if(record&&record.helpid) {
+      let helpuser = record.helpid;
+      database.data = database.data.filter((x) => x.id != interaction.user.id);
+      return await interaction.reply(`<@${interaction.user.id}> is no longer helping <@${helpuser}>. Sadge.`);
     }
-    return await interaction.reply({content:`Who are you even talking to?`, ephemeral: true})
+    return await interaction.reply({content:`You're not helping anyone!`, ephemeral: true});
   }
 );
--- src/modules/listen.js
+++ src/modules/listen.js
@@ -2,9 +2,26 @@ import { defineEvent } from "strife.js";
 import { database } from "./punishmentdb.js";
 let INSULTS = ["KEEP WORKING", "WORK HARDER", "WORK FASTER", "MORE COAL", "FUCK YOU!"];
 defineEvent("messageCreate", async (message) => {
-  let coalCount =
-    message.content.split("<:coal:1204805721007595550>").length - 1;
-  if (database.data.some((x) => x.id == message.author.id)) {
+  let coalCount = message.content.split("<:coal:1204805721007595550>").length - 1;
+  let thishelpid = database.data.find((x) => x.helpid && x.id == message.author.id);
+  
+  if(thishelpid) {
+    let targetuser = database.data.find((x) => x.id == thishelpid.helpid);
+    if (coalCount < 1) {
+      return message.reply(`Why aren't you helping <@${targetuser.id}>?`);
+    }
+    targetuser.coal -= coalCount;
+    database.data = [
+      ...database.data.filter((x) => x.id != targetuser.id),
+      ...(targetuser.coal >= 1 ? [targetuser] : []),
+    ];
+    if (targetuser.coal < 1) {
+      database.data = database.data.filter((x) => (x.id != message.author.id||!x.helpid))
+      return await message.reply(`<@${targetuser.id}> is free, thanks to <@${message.author.id}>!`);
+    }
+    return await message.reply(`You helped <@${targetuser.id}> mine ${coalCount} coal!`);
+  
+  } else if (database.data.some((x) => x.id == message.author.id)) {
     if (coalCount < 1) {
       return message.reply("GET BACK TO WORK");
     }
