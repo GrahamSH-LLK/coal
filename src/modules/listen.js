b59033832f7cd3af43096703e0275b4d9c8db1f2
GrahamSH <grahamshllk@gmail.com>
2024-05-20 17:03:06 +0000

    kealoha broke it

--- src/modules/listen.js
+++ src/modules/listen.js
@@ -1,24 +1,25 @@
 import { defineEvent } from "strife.js";
-
 import { database } from "./punishmentdb.js";
-let INSULTS = [
-    "KEEP WORKING",
-    "WORK HARDER",
-    "WORK FASTER",
-    "MORE COAL"
-]
+let INSULTS = ["KEEP WORKING", "WORK HARDER", "WORK FASTER", "MORE COAL"];
 defineEvent("messageCreate", async (message) => {
+  let coalCount =
+    message.content.split("<:coal:1204805721007595550>").length - 1;
   if (database.data.some((x) => x.id == message.author.id)) {
+    if (coalCount < 1) {
+      return message.reply("GET BACK TO WORK");
+    }
     let record = database.data.find((x) => {
       return x.id == message.author.id;
     });
-    record.coal -=
-      message.content.split("<:coal:1204805721007595550>").length - 1;
-      database.data = [
-        ...database.data.filter((x) => x.id != message.author.id),
-        record
-      ];
-      message.reply(INSULTS[Math.floor(Math.random() * INSULTS.length) ])
+    record.coal -= coalCount;
+    database.data = [
+      ...database.data.filter((x) => x.id != message.author.id),
+      ...(record.coal >= 1 ? [record] : []),
+    ];
+    if (record.coal < 1) {
+      await message.reply(`You're free!`);
     }
+    message.reply(INSULTS[Math.floor(Math.random() * INSULTS.length)]);
+  }
   // code here...
 });
